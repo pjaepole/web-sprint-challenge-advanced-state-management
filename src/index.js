b3e6bc195962720fa37343213117847fcf14b956
Jaehun Park <pjaehun4122@gmail.com>
2021-10-08 12:38:02 -0700

    connected application to thunk logger middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import logger from 'redux-logger';
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import reducer from './reducers'
 import "./index.css";
 import App from "./App";
-const store = createStore(reducer);
+const store = createStore(reducer,applyMiddleware(logger,thunk));
 const { worker } = require('./mocks/browser');
 worker.start();
 
